69f7cdaebc265cabc3ac31538963910db74e2d05
incredulous <incredulous97@gmail.com>
2024-03-30 13:28:00 +0800

    feat | get eth-usd price

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { ExtractorService } from './services/extractor.service';
+import { PriceService } from './services/price.service';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -8,7 +9,8 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-ExtractorService.getSingleton().init();
+// ExtractorService.getSingleton().init();
+PriceService.getSingleton().getEthUsdConversionRate('1711761887');
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
---
+++ src/services/price.service.ts
@@ -0,0 +1,36 @@
+import axios, { AxiosInstance } from 'axios';
+
+export class PriceService {
+  static singleton: PriceService;
+
+  private client: AxiosInstance;
+
+  constructor() {
+    this.client = axios.create({
+      baseURL: 'https://api.binance.com/api',
+    });
+  }
+
+  static getSingleton(): PriceService {
+    if (!PriceService.singleton) {
+      PriceService.singleton = new PriceService();
+    }
+    return PriceService.singleton;
+  }
+
+  async getEthUsdConversionRate(transactionTimestamp: string): Promise<number> {
+    try {
+      const transactionDate = new Date(parseInt(transactionTimestamp) * 1000);
+      const startTime = new Date(transactionDate.getFullYear(), transactionDate.getMonth(), transactionDate.getDate()).getTime();
+      const endTime = new Date(transactionDate.getFullYear(), transactionDate.getMonth(), transactionDate.getDate() + 1).getTime();
+
+      const response = await this.client.get(`https://api.binance.com/api/v3/klines?symbol=ETHUSDT&interval=1d&startTime=${startTime}&endTime=${endTime}&limit=1`);
+
+      const ethUsdClosingPrice = parseFloat(response.data[0][4]);
+
+      return ethUsdClosingPrice;
+    } catch (error: any) {
+      throw 'ERROR_GET_PRICE';
+    }
+  }
+}
